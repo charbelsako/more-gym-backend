d984a44a572172d819f9df522e29f9cbf6c215e6
Charbel Sarkis <charbelsako@gmail.com>
2024-01-12 16:40:06 +0200

    Fixes bug in reset user pass

--- routes/admin.js
+++ routes/admin.js
@@ -8,37 +8,43 @@ const User = require('../models/User');
 
 // Middleware to check if the user is an admin
 const { isAdmin } = require('../middleware/roles');
+const { authenticateToken } = require('../middleware/authenticate');
 
-router.post('/reset-user-password', isAdmin, async (req, res) => {
-  try {
-    const { username, email, newPassword } = req.body;
+router.post(
+  '/reset-user-password',
+  authenticateToken,
+  isAdmin,
+  async (req, res) => {
+    try {
+      const { username, email, newPassword } = req.body;
 
-    // Check if either username or email is provided
-    if (!username && !email) {
-      return res
-        .status(400)
-        .json({ message: 'Provide either username or email' });
+      // Check if either username or email is provided
+      if (!username && !email) {
+        return res
+          .status(400)
+          .json({ message: 'Provide either username or email' });
+      }
+
+      // Find the user by username or email
+      const user = await User.findOne({ $or: [{ username }, { email }] });
+
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+
+      // Hash the new password before updating
+      const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+
+      // Update the password in the database
+      user.password = hashedNewPassword;
+      await user.save();
+
+      res.status(200).json({ message: 'User password reset successful' });
+    } catch (error) {
+      console.error('Error during user password reset:', error);
+      res.status(500).json({ message: 'Internal server error' });
     }
-
-    // Find the user by username or email
-    const user = await User.findOne({ $or: [{ username }, { email }] });
-
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    // Hash the new password before updating
-    const hashedNewPassword = await bcrypt.hash(newPassword, 10);
-
-    // Update the password in the database
-    user.password = hashedNewPassword;
-    await user.save();
-
-    res.status(200).json({ message: 'User password reset successful' });
-  } catch (error) {
-    console.error('Error during user password reset:', error);
-    res.status(500).json({ message: 'Internal server error' });
   }
-});
+);
 
 module.exports = router;
