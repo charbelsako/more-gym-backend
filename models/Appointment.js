38b23970b047da1b60e20b4fd8fb74b149325464
Charbel Sarkis <charbelsako@gmail.com>
2024-01-19 16:16:46 +0200

    renames appointments to appointment

--- models/Appointment.js
+++ models/Appointment.js
@@ -1,16 +1,12 @@
 const mongoose = require('mongoose');
-
-const status = {
-  CONFIRMED: 'Confirmed',
-  CANCELLED: 'Cancelled',
-};
+const { appointmentStatus } = require('../constants');
 
 const appointmentSchema = new mongoose.Schema({
   trainerId: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
   date: Date,
   time: Number, // 8 or 9 or 10 ....
-  status: { type: String, enum: Object.values(status) },
+  status: { type: String, enum: Object.values(appointmentStatus) },
 });
 
 module.exports = Appointments = mongoose.model(
