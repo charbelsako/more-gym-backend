d15d70f831ffb3d7b8ca1e5b5cce835e01d23e21
Charbel Sarkis <charbelsako@gmail.com>
2024-02-15 14:40:56 +0200

    Adds price to membership

--- models/Membership.js
+++ models/Membership.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const membershipSchema = new mongoose.Schema({
   type: { type: mongoose.Schema.Types.ObjectId, ref: 'packageType' },
   capacity: { type: mongoose.Schema.Types.ObjectId, ref: 'sessionType' },
+  price: Number,
 });
 
 module.exports = Membership = mongoose.model('membership', membershipSchema);
