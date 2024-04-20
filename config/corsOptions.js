fcd48945c0243d79637f4ff7e6cb0a459deef63f
Charbel Sarkis <charbelsako@gmail.com>
2024-04-20 13:34:25 +0300

    adds console log for debugging

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,12 +2,13 @@ const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
   origin: (origin, callback) => {
+    console.log(origin);
     if (allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else if (process.env.NODE_ENV === 'development' && !origin) {
       callback(null, true);
     } else {
-	 console.log(origin);
+      console.log(origin);
       callback(new Error('Not allowed by CORS'));
     }
   },
